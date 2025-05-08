1cc89d4e42577094d1a8e4605018f433b7ca2001
Mustafa <mostafamohamed12434@gmail.com>
2025-05-08 13:53:51 +0300

    vercel

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ app.use("/users", user_routes);
 app.use("/products", product_routes);
 app.use("/admins", admin_routes);
 app.use((req, res) => {
-  console.log(`NotFound`);
+  return res.status(400).json({
+    status: 400,
+    data: { data: null, message: "invalid route" },
+  });
 });
-app.listen(3000, () => {
-  console.log(`Online`);
-});
+// app.listen(3000, () => {
+//   console.log(`online`);
+// });
+modules.exports = app;
