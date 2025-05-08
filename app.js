0156a91fa06223b482f9aaa844c845b0d4950ba4
GIGZ <titoadham706@gmail.com>
2025-05-08 20:15:19 +0300

    linked to mongoDb & added getProducts, getProduct, updateProduct

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const express = require("express");
+const cors = require("cors");
 const { admin_routes } = require("./routes/admins");
 const { product_routes } = require("./routes/products");
 const { user_routes } = require("./routes/users");
+// =========
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+.catch(err => console.error(err));
+// =========
+
+
+
 const app = express();
+app.use(cors());
 
 app.use("/users", user_routes);
 app.use("/products", product_routes);
 app.use("/admins", admin_routes);
+
+app.use(express.json()); // for parsing application/json
+
 app.use((req, res) => {
   return res.status(400).json({
     status: 400,
     data: { data: null, message: "invalid route" },
   });
 });
-// app.listen(3000, () => {
-//   console.log(`online`);
-// });
-modules.exports = app;
+
+// when using the listen method, comment out the export statement
+app.listen(3000, () => {
+  console.log(`online`);
+});
+
+// modules.exports = app;
--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,35 @@
-const getProducts = async () => {
+const product = require("../models/product");
+
+const getProducts = async (req, res) => {
+  const products = await product.find();
+    res.json(products)
   console.log(`getProducts`);
 };
-const getSingleProducts = async () => {
-  console.log(`getProducts`);
+
+const getSingleProducts = async (req, res) => {
+  const Product = await product.findById(req.params.id);
+  if (!Product) return res.status(404).json({ message: 'Product not found' });
+  res.json(Product);
+  console.log(`getSingleProduct`);
 };
+
+// create a new product
 const postProducts = async () => {
   console.log(`postProducts`);
 };
+
+
+
+
 const deleteProducts = async () => {
   console.log(`deleteProducts`);
 };
-const putProducts = async () => {
+
+// update a product
+const putProducts = async (req, res) => {
+  const updated = await product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  if (!updated) return res.status(404).json({ message: 'Product not found' });
+  res.json(updated);
   console.log(`putProducts`);
 };
 module.exports = {
